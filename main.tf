f71e97da90b90ea10bc6bc3d3ba122480c95a9f7
shiraIdan <93222622+shiraIdan@users.noreply.github.com>
2022-11-06 17:47:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,15 +12,22 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/24"
   tags = {
     "Name" = "ShiraIdan-dev-vpc"
   }
 }
 
+resource "aws_internet_gateway" "in_gw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "shira_in_gw"
+  }
+}
+
 resource "aws_subnet" "Subnet" {
-  vpc_id     = aws_vpc.dev_vpc.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.0.0/27"
   tags = {
     "Name" = "ShiraIdan-k8s-subnet"
@@ -28,3 +35,20 @@ resource "aws_subnet" "Subnet" {
   }
 
 }
+
+resource "aws_route" "routeIGW" {
+  route_table_id         = aws_vpc.vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.in_gw.id
+}
+
+# Setting up the network interface for machines 
+resource "aws_network_interface" "IntCard" {
+  subnet_id   = aws_subnet.Subnet.id
+  private_ips = ["10.0.0.10"]
+
+  tags = {
+    "name" = "shira_IntCard"
+  }
+}
+
